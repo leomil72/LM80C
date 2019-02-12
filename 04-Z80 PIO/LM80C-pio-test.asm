4350d1f85934b35c472ff98b9d40c3f6a9e7120b
leo72 <leonardo@leonardomiliani.com>
2019-02-12 17:43:40 +0100

    Code revision

--- 04-Z80 PIO/LM80C-pio-test.asm
+++ 04-Z80 PIO/LM80C-pio-test.asm
@@ -4,15 +4,16 @@
 ; designed by Leonardo Miliani. More info at
 ; www DOT leonardomiliani DOT com
 ; ---------------------------------------------------------
-; Code: by Leonardo Miliani
+; Code by Leonardo Miliani
+; Compiler: ZASM assembler 4.2.4-macos10.12
 ;
 ; Revisions:
 ; 0.1 - 20190202 - First version - just a test
 ; 0.2 - 20190206 - Stable version - working
+; 1.0 - 20190212 - Code revision - stable version
 ;
 ; ---------------------------------------------------------
 ;
-
 
 ; labels defining
 DATAREGA    equ 00000000b
@@ -20,18 +21,17 @@ DATAREGB    equ 00000001b
 CTRLREGA    equ 00000010b
 CTRLREGB    equ 00000011b
 
-; this line instructs the assembler to prepare a file for a ROM target
+; this directive instructs the assembler to prepare a file for a ROM target
 ; meaning that blank cells will be filled up with 0xff
 #target rom
 
-; this line instructs the assembler to compile while taking account of
-; the start of the code fixed at 0x0000h and that the file shall be
-; 0xff bytes in size
+; this directive instructs the assembler to compile while taking account of
+; the start of the code that must be set to 0x0000h and that the file has a fixed size
 #code BOOT, 0000h, 00ffh
 
 reset:  ; this corresponds to the RESET vector (0000h)
 
-        ; a little delay
+        ; let's insert a little delay
         ld d, 0xff
 loop3:
         ld b,0xff
@@ -40,7 +40,7 @@ loop4:
         dec d
         jp nz, loop3
 
-        ;program the PIO
+        ; let's program the PIO
         ld a,11001111b      ; mode 3 (bit control)
         out (CTRLREGB),a
         ld a,00000000b      ; set pins of port B to OUTPUT
@@ -52,6 +52,7 @@ noexit:
         ; send the pattern to the PIO
         ld a,e
         out (DATAREGB),a
+
         ; a little delay
         ld d,0x80
 loop1:
@@ -60,6 +61,7 @@ loop2:
         djnz loop2
         dec d
         jp nz, loop1
+        
         ; increment the pattern, so that it cycles from 0 to 255 repeatedly
         inc e
         ; sent the new pattern to the PIO
